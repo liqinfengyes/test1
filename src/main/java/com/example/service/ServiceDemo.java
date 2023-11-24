747302d2778ba4858635eb46f576d273bfd001dc
lqf <1842513679@qq.com>
2023-11-24 15:04:34 +0800

    master1_test

--- src/main/java/com/example/service/ServiceDemo.java
+++ src/main/java/com/example/service/ServiceDemo.java
@@ -51,6 +51,7 @@ public class ServiceDemo {
         }
         System.out.println("1");
         System.out.println("211");
+        System.out.println("2111");
         sqlSession.close();
 //        System.out.println(studentList);
 //        List<Student> studentList = studentMapper.findByCondition(student);
